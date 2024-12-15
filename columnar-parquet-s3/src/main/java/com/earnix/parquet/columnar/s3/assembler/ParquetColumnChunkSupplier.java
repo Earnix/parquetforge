0dc487314061ad1750b51b12df7e934d9e5a41ff
Andrew Pikler <andrew.pikler@earnix.com>
2024-12-16 00:45:02 +0200

    documentation

--- columnar-parquet-s3/src/main/java/com/earnix/parquet/columnar/s3/assembler/ParquetColumnChunkSupplier.java
+++ columnar-parquet-s3/src/main/java/com/earnix/parquet/columnar/s3/assembler/ParquetColumnChunkSupplier.java
@@ -11,13 +11,30 @@ import java.io.InputStream;
  */
 public interface ParquetColumnChunkSupplier
 {
+	/**
+	 * @return the number of rows in this column chunk
+	 */
 	long getNumRows();
 
+	/**
+	 * @return the compressed length of this column chunk including page headers
+	 */
 	long getCompressedLength();
 
+	/**
+	 * @return the column chunk metadata for this column. Note that this creates a deep copy of the metadata every time
+	 * 		it is called to ensure that the metadata is not accidentally mutated, so avoid muliple repetitive calls
+	 */
 	ColumnChunk getColumnChunk();
 
+	/**
+	 * @return an input stream for the bytes within this chunk
+	 * @throws IOException on failure
+	 */
 	InputStream openInputStream() throws IOException;
 
+	/**
+	 * @return the column descriptor for this column
+	 */
 	ColumnDescriptor getColumnDescriptor();
 }
